134bd5060ebfcd59f30dded19e22a70cf7547892
Igor Salnikov <isalnikov1@gmail.com>
2017-02-06 13:21:41 +0300

    https://github.com/isalnikov/GwtSpringBoot

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# GwtSpringBoot
-Gwt Spring Boot simple
+#GwtSpringBoot
+
+
+Simple example  SPRING BOOT + GWT 
+
+No web.xml  , only java
+
+1. Create maven gwt archetype 
+   https://gwt-maven-plugin.github.io/gwt-maven-plugin/user-guide/archetype.html
+
+   put Main.gwt.xml to correct folder in resource
+
+2. pom.xml add gwt config  , set webappDirectory  (mo war dir)
+
+3. in Main.gwt.xml add
+    <module rename-to="gwtjs"> <!-- dist name -->
+    <set-property name="user.agent" value="safari"/> <!-- only google chrome -->
+
+4. put index.html to static 
+
+5. TODO debug gwt
+
+6. Rest API and GWT|GXT GUI 
--- src/main/java/com/isalnikov/gwtboot/client/MainEntryPoint.java
+++ src/main/java/com/isalnikov/gwtboot/client/MainEntryPoint.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.isalnikov.gwtboot.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.event.dom.client.ClickEvent;
 
-/**
- * Main entry point.
- *
- * @author Alexander Chuprynov <achuprynov@gmail.com>
- */
 public class MainEntryPoint implements EntryPoint {
 
  
@@ -28,7 +17,7 @@ public class MainEntryPoint implements EntryPoint {
         final Label label = new Label("Hello, GWT !!!");
         final Button button = new Button("Click me!");
         
-        button.addClickHandler((ClickEvent event) -> {
+        button.addClickHandler((event) -> {
             label.setVisible(!label.isVisible());
         });
         
